84af4d9b6f91772f9b7903388b8b9dcf8b5c4fe5
Italo Costa <italocc16@gmail.com>
2023-04-14 11:59:11 -0300

    feature: Building calendar layour using global tailwind configurations

--- src/components/CalendarComponent/index.tsx
+++ src/components/CalendarComponent/index.tsx
@@ -1,11 +1,26 @@
 /* import { useState } from 'react' */
+import { CaretLeft, CaretRight } from '@phosphor-icons/react'
 import { Calendar } from 'react-calendar'
+/* import './calendarStyle.css' */
 export const CalendarComponent = () => {
   /*  const [value, onChange] = useState(new Date()) */
+
+  const calendarConfig = {
+    selectRange: true,
+    prevAriaLabel: 'prev calendar button',
+    nextAriaLabel: 'next calendar button',
+    prevLabel: <CaretLeft weight="bold" size={20} />,
+    nextLabel: <CaretRight weight="bold" size={20} />,
+    prev2Label: null,
+    next2Label: null,
+  }
+
   return (
     <div>
       <Calendar
         onChange={(dateSelected: any) => console.log(dateSelected)}
+        className="calendar-style-customized"
+        {...calendarConfig}
         /* value={value} */
       />
     </div>
