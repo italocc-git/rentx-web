5758053a606dffbd6cfae32469876f45f82b89a1
Italo Costa Cavalcante <italo.cavalcante@euqueroinvestir.com>
2023-11-13 10:09:41 -0300

    feature: adding graphql plugin on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
+import Graphql from '@rollup/plugin-graphql'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Graphql()],
 })
