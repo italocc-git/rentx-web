ad026425dfc41482d12558bebe4fce841869bdea
Italo Costa Cavalcante <italocc16@gmail.com>
2024-04-14 18:30:36 -0300

    feature: adding test example

--- cypress/e2e/hello-world.cy.ts
+++ cypress/e2e/hello-world.cy.ts
@@ -1,16 +1,16 @@
-describe('Cars List Page', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
-  it('should render the page', () => {
-    cy.contains('Começar agora').click()
+// describe('Cars List Page', () => {
+//   beforeEach(() => {
+//     cy.visit('/')
+//   })
+//   it('should render the page', () => {
+//     cy.contains('Começar agora').click()
 
-    cy.location('pathname').should('include', '/inicio')
-  })
-  it('should render the first car page', () => {
-    cy.contains('Começar agora').click()
+//     cy.location('pathname').should('include', '/inicio')
+//   })
+//   it('should render the first car page', () => {
+//     cy.contains('Começar agora').click()
 
-    cy.get('a[href^="/inicio/carro"]').first().click()
-    cy.location('pathname').should('include', '/carro')
-  })
-})
+//     cy.get('a[href^="/inicio/carro"]').first().click()
+//     cy.location('pathname').should('include', '/carro')
+//   })
+// })
